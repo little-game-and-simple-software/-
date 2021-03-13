5754cf57ceed9025ccc274163c68618cb8e1c35d
2439905184 <suchenglong018@126.com>
2021-03-13 16:42:14 +0800

    1.添加了角色界面，添加了主站宣传视频。 2.角色界面动画效果参考了明日方舟网站的动画注：明日方舟官网使用的是vue，而我用的是jquery

--- character.js
+++ character.js
@@ -1,4 +1,50 @@
 $(function()
 {
-    //$("#lava").attr("img","img/lava.png")
+    $("#info").slideUp()
+    function char_slide(id)
+    {
+        $("#"+id).animate
+        ({
+            left:"300px",
+            width:"600px",
+            })
+            $("#info").css({visibility:"visible"})
+            $("#info").css({left:"700px"})
+            $("#info").slideDown()
+    }
+    $("#lava").click(function()
+    {
+        $("#info h3").text("炎熔")
+        char_slide("lava")
+    })
+    // 德克萨斯
+    $("#fire").click(function()
+    {
+        $("#info h3").text("德克萨斯")
+        $("#info h4").text("职业：先锋")
+        $("#info").css({visibility:"visible"})
+        $(this).animate
+        ({
+            left:"300px",
+            width:"400px",
+        })
+        $("#info").css({visibility:"visible"})
+        $("#info").css({left:"700px",top:"400px"})
+        $("#info").slideDown()
+    })
+    $("#close").click(function()
+    {
+        $("#lava").animate(
+        {
+                left:"100px",
+                width:"100px",
+        })
+        $("#fire").animate(
+        {
+                left:"0px",
+                width:"100px",
+        })
+        $("#info").slideUp()
+    })
+
 })
